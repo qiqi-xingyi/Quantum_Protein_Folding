ad05822946e126b17b06333bd77ff437eb42fbc6
qiqi-xingyi <yuqizhang247@gmail.com>
2024-10-16 16:53:53 -0400

    qiskit change version

--- protein_folding/qubit_op_builder.py
+++ protein_folding/qubit_op_builder.py
@@ -1,12 +1,3 @@
-# (C) Copyright IBM 2021, 2022.
-#
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
-#
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
 """Builds qubit operators for all Hamiltonian terms in the protein folding problem."""
 from typing import Union
 
